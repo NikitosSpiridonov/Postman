fcb0422dd43f530a81eafdefc00f5fe91ab63031
Nikita Spiridonov <spiridonov.n.i@yandex.ru>
2024-04-11 19:36:59 +0300

    Initial commit

---
+++ src/test/java/Postman.java
@@ -0,0 +1,25 @@
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Postman {
+    @Test
+    void shouldPost() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello world") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello world"));
+
+
+    }
+}
